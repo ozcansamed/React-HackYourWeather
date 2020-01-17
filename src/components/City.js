536e2efc51034b92b954d3756510bd2227b3d674
ozcansamed <ozcansamed61@gmail.com>
2020-01-17 03:56:50 +0100

    finished project week-1

---
+++ src/components/City.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function City({ data }) {
+  return (
+    <div className='city'>
+      <h1>{`${data.name}, ${data.sys.country}`}</h1>
+      <h3>{data.weather[0].main}</h3>
+      <h4>{data.weather[0].description}</h4>
+      <p>{`min temp: ${data.main.temp_min}`}</p>
+      <p>{`max temp: ${data.main.temp_max}`}</p>
+      <p>{`location: ${data.coord.lon},${data.coord.lat}`}</p>
+    </div>
+  );
+}
