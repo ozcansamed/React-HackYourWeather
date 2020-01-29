9ee80c3dc66c9deb48d65353b7607ffdb1459c65
ozcansamed <ozcansamed61@gmail.com>
2020-01-29 21:42:35 +0100

    changes after review

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,45 +12,8 @@ export default function SearchForm({
         name='city'
         onChange={onHandleSearchInputChange}
         placeholder='Search City...'
-        style={{ margin: "10px", padding: "5px" }}
       />
       <button>Search</button>
     </form>
   );
 }
-
-/*
- <input type='submit' className='submit' />
-import React, { useState, useEffect } from "react";
-
-export default function SearchBar() {
-  return (
-    <div>
-      <form>
-        <input type='text' name='city' placeholder='Search City'></input>
-        <button type='submit'>Search</button>
-      </form>
-    </div>
-  );
-}
-
-export default function Emoji() {
-  const [inputValue, setInputValue] = useState("");
-
-  return (
-    <div>
-      <input
-        value={inputValue}
-        onChange={event => setInputValue(event.target.value)}
-      />
-      <p>{translate.translate(inputValue)}</p>
-    </div>
-  );
-}
-
-export function Button({ onClick, label }) {
-  return <button>{label}</button>;
-}
-
- 
-*/
